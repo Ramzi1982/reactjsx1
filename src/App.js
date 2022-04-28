7a39a2a3ca0d2b4e9b899acf82e8928126fec364
ramzi <r.hamzaoui@nextwebplus.fr>
2022-04-28 16:52:45 +0200

    reactjsx1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './style.css';
+import Img2 from './img2.jpg'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div
+       style={{border:"solid 1px noir",maxWidth:"100vw"}}>
+
+<h1 className="title red">رمضان كريم</h1>
+
+<br/>
+
+<img className='imageInSrc' src={Img2} alt="Allah"/> 
+
+<br/>
+<h1 className="title red">كل عام و انتم بخير</h1>
+
+<img className='imageInPublic' src='/img1.jpg' alt="Muhamed"/>
+
+<video width="350" height="280" controls className='video'>
+
+  <source src="/maVideo.mp4" type="video/mp4"/></video>
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+    
+    
   );
 }
 
